9f361f10fc90a254d6b1acc7d6ee745c3aec03b0
jhs512 <jangka512@gmail.com>
2025-03-27 14:27:57 +0900

    work

---
+++ src/main/kotlin/com/back/Kp20250327Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kp20250327Application
+
+fun main(args: Array<String>) {
+    runApplication<Kp20250327Application>(*args)
+}
