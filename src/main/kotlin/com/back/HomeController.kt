4726d5be5038f8a9ccf2f70e43381398c7682183
jhs512 <jangka512@gmail.com>
2025-03-27 14:34:18 +0900

    make bucket, show all bucket list

---
+++ src/main/kotlin/com/back/HomeController.kt
@@ -0,0 +1,13 @@
+package com.back
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.s3.S3Client
+
+@RestController
+class HomeController(
+    private val s3Client: S3Client,
+) {
+    @GetMapping("/")
+    fun main() = s3Client.listBuckets().buckets().map { it.name() }
+}
