fdf879019b44538c8d3e7f772ca62437d401945c
jhs512 <jangka512@gmail.com>
2025-03-27 14:49:17 +0900

    work

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -3,6 +3,7 @@ package com.back
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
+import java.io.File
 
 @RestController
 class HomeController(
@@ -10,4 +11,26 @@ class HomeController(
 ) {
     @GetMapping("/")
     fun main() = s3Client.listBuckets().buckets().map { it.name() }
+
+    @GetMapping("/upload1")
+    fun upload1(): String {
+        val filePath = """
+            C:\Users\jhs512\Pictures\Screenshots\스크린샷 2024-01-29 092348.png
+        """.trimIndent()
+
+        val bucketName = "bucket-jhs512-01"
+        val key = "screenshots/스크린샷 2024-01-29 092348.png"
+
+        val file = File(filePath)
+        val requestBody = software.amazon.awssdk.core.sync.RequestBody.fromFile(file)
+
+        val putObjectRequest = software.amazon.awssdk.services.s3.model.PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(key)
+            .build()
+
+        s3Client.putObject(putObjectRequest, requestBody)
+
+        return "완료"
+    }
 }
